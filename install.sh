c4e3680af1c522858b2a834ddbaebd91ab0fe0f3
Vinicius Henrique Marangoni <vinicius_marangoni1@hotmail.com>
2017-10-25 00:02:32 -0200

    first commit

---
+++ install.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+install_pacaur(){
+	mkdir -p /tmp/pacaur_install
+	cd /tmp/pacaur_install
+	
+	sudo pacman -S binutils make gcc fakeroot pkg-config --noconfirm --needed
+	sudo pacman -S expac yajl git --noconfirm --needed
+	
+	if [ ! -n "$(pacman -Qs cower)" ]; then
+	    curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
+	    makepkg PKGBUILD --skippgpcheck --install --needed
+	fi
+
+	if [ ! -n "$(pacman -Qs pacaur)" ]; then
+	    curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+	    makepkg PKGBUILD --install --needed
+	fi
+
+	cd ~
+	rm -r /tmp/pacaur_install 
+}
+
+install_tldr(){
+	if [ ! -e /usr/local/bin/tldr ]; then
+		echo "[+] Installing tldr..."
+		location=/usr/local/bin/tldr
+		sudo wget -qO $location https://raw.githubusercontent.com/pepa65/tldr-bash-client/master/tldr
+		sudo chmod +x $location
+		$location --update
+	fi
+}
+
+arch_linux_gui_dependent(){
+    pacaur -S terminator --needed --noconfirm
+}
+
+# Arch Linux
+arch_linux_install(){
+	echo "[+] Updating..."
+    sudo pacman -Syu --noconfirm
+	sudo pacman -S curl wget gvim nano --needed --noconfirm
+
+	if [ -z "$(pacman -Qs pacaur)" ]; then
+		echo "[+] Installing pacaur..."
+		install_pacaur
+	fi
+	
+	pacaur -S base-devel --needed --noconfirm
+	pacaur -S git python2-pip --needed --noconfirm
+    pacaur -S yadm-git --needed --noconfirm
+    pacaur -S tmux --needed --noconfirm
+	install_tldr
+    pacaur -S autojump --needed --noconfirm
+    
+
+
+
+
+    printf "${GREEN}Should install GUI dependent packages? [y/N]: ${NC}"
+    read ans
+    ans=${ans:-N}
+
+    if [ $ans = "y" -o $ans = "Y" ]; then
+        arch_linux_gui_dependent
+    fi
+}
+
+ubuntu_linux_gui_dependent(){
+	sudo apt install terminator
+}
+
+# Ubuntu Linux
+ubuntu_linux_install(){
+	echo "[+] Updating..."
+	sudo apt update && sudo apt upgrade -y
+	sudo apt install curl wget vim-gtk -y
+	sudo apt install build-essential -y
+	sudo apt install git python-pip
+	sudo apt install tmux -y
+    #pacaur -S yadm-git --needed --noconfirm
+	install_tldr
+	sudo apt install autojump
+
+	printf "${GREEN}Should install GUI dependent packages? [y/N]: ${NC}"
+	read ans
+	ans=${ans:-N}
+
+    if [ $ans = "y" -o $ans = "Y" ]; then
+		ubuntu_linux_gui_dependent
+    fi
+} 
+
+dotfiles_install(){
+    yadm clone https://github.com/viniciusmarangoni/dotfiles
+    yadm status
+}
+
+##############################
+#######   Entrypoint   #######
+##############################
+
+if [ "$EUID" = "0" ]; then
+    echo -e "${RED}[-] You shoul'd not execute this script as root!${NC}"
+    exit 1
+fi
+
+# Source variables in os-release
+. /etc/os-release
+
+if [ "$NAME" = "Arch Linux" ]; then
+    arch_linux_install
+fi
+
+dotfiles_install
